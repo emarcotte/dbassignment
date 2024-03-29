731afbc50bbdf17728a5b87dbb521d1a4ca9747c
Eugene Marcotte <emarcotte@gmail.com>
2024-03-29 16:58:42 -0400

    Example #2 works

--- tests/examples.ts
+++ tests/examples.ts
@@ -1,9 +1,10 @@
 import { describe, expect, test } from '@jest/globals';
 import { AssignmentDB } from '../src/database';
 
+// These tests are ripped from the original document and exercise
+// the database API, not the CLI.
 describe('devoted tech assignment', () => {
-    test('example 1 test', () => {
-        // This test is ripped from the original document and exercises the database API, not the CLI.
+    test('example 1', () => {
         const db = new AssignmentDB();
         // >> GET a
         // NULL
@@ -44,6 +45,29 @@ describe('devoted tech assignment', () => {
         // >> GET B
         // NULL
         expect(db.get("B")).toBeUndefined();
+        // >> END
+    });
+
+    test('example 2', () => {
+        const db = new AssignmentDB();
+        // >> SET a foo
+        expect(db.set("a", "foo")).toEqual("foo");
+        // >> SET a foo
+        expect(db.set("a", "foo")).toEqual("foo");
+        // >> COUNT foo
+        // 1
+        expect(db.count("foo")).toEqual(1);
+        // >> GET a
+        // foo
+        expect(db.get("a")).toEqual("foo");
+        // >> DELETE a
+        expect(db.delete("a")).toEqual("foo");
+        // >> GET a
+        // NULL
+        expect(db.get("a")).toBeUndefined();
+        // >> COUNT foo
+        // 0
+        expect(db.count("a")).toEqual(0);
         // >> END
     });
 });
