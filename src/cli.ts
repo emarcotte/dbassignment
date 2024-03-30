6ad2010df108b51cd75dc97f8c580e212345bcdc
Eugene Marcotte <emarcotte@gmail.com>
2024-03-30 12:10:18 -0400

    Add the ui and some tests

---
+++ src/cli.ts
@@ -0,0 +1,123 @@
+import { AssignmentDB } from "./database";
+import * as readline from 'node:readline/promises';
+import { stdin, stdout } from 'node:process';
+
+const command_regex = /^([A-Z]+)(.*)/;
+const set_args = /^([^s]+)\s(.+)/;
+const get_args = /^([^s]+)/;
+const delete_args = /^([^s]+)/;
+
+const handlers: Record<string, (db: AssignmentDB, args: string, output: NodeJS.WritableStream) => boolean> = {
+    SET: set_handler,
+    GET: get_handler,
+    COUNT: count_handler,
+    DELETE: delete_handler,
+    BEGIN: begin_handler,
+    ROLLBACK: rollback_handler,
+    COMMIT: commit_handler,
+    END: end_handler,
+}
+
+function end_handler(): boolean {
+    return true;
+}
+
+function count_handler(db: AssignmentDB, value: string, output: NodeJS.WritableStream): boolean {
+    const count = db.count(value)
+    output.write(`${count}\n`);
+    return false;
+}
+
+function delete_handler(db: AssignmentDB, args: string, output: NodeJS.WritableStream): boolean {
+    const match = delete_args.exec(args);
+    if (!match) {
+        output.write("Invalid DELETE\n");
+    }
+    else {
+        db.delete(match[1]);
+    }
+    return false;
+}
+
+function commit_handler(db: AssignmentDB): boolean {
+    db.commit();
+    return false;
+}
+
+function begin_handler(db: AssignmentDB): boolean {
+    db.begin();
+    return false;
+}
+
+function rollback_handler(db: AssignmentDB, _: string, output: NodeJS.WritableStream): boolean {
+    const t = db.rollback();
+    if (!t) {
+        output.write("TRANSACTION NOT FOUND\n");
+    }
+    return false;
+}
+
+function get_handler(db: AssignmentDB, args: string, output: NodeJS.WritableStream): boolean {
+    const match = get_args.exec(args);
+    if (!match) {
+        output.write("Invalid GET\n");
+    }
+    else {
+        const v = db.get(match[1]);
+        if (v === undefined) {
+            output.write(`NULL\n`);
+        }
+        else {
+            output.write(`${v}\n`);
+        }
+    }
+
+    return false;
+}
+
+function set_handler(db: AssignmentDB, args: string, output: NodeJS.WritableStream): boolean {
+    const match = set_args.exec(args);
+    if (!match) {
+        output.write("Invalid SET\n");
+    }
+    else {
+        db.set(match[1], match[2]);
+    }
+    return false;
+}
+
+export async function main(
+    db: AssignmentDB,
+    input: NodeJS.ReadableStream,
+    output: NodeJS.WritableStream,
+) {
+    const ui = readline.createInterface({
+        input,
+        output,
+    });
+    let exit = false;
+
+    while (!exit) {
+        const line = await ui.question(">> ");
+        const match = command_regex.exec(line);
+        if (!match) {
+            output.write(`dont know what to do with ${line}\n`);
+        }
+        else {
+            const args = (match[2] || "").trim();
+            const handler = handlers[match[1]];
+            if (!handler) {
+                output.write(`Invalid command ${match[1]}\n`)
+            }
+            else {
+                exit = handler(db, args, output);
+            }
+        }
+    }
+    output.end();
+    ui.close();
+}
+
+if (require.main === module) {
+    main(new AssignmentDB(), stdin, stdout);
+}
---
+++ tests/ui.ts
@@ -0,0 +1,122 @@
+import { describe, expect, test } from '@jest/globals';
+import { AssignmentDB } from '../src/database';
+import { main } from '../src/cli';
+import { PassThrough } from 'stream';
+import { stdin as mockStdIn, MockSTDIN } from 'mock-stdin';
+
+async function inputs(stdin: MockSTDIN, inputs: string[]): Promise<void> {
+    for (const i of inputs) {
+        await new Promise(process.nextTick);
+        stdin.send(`${i}\n`);
+    }
+}
+
+async function readAll(stream: PassThrough): Promise<string> {
+    let b = Buffer.from([]);
+    for await (const chunk of stream) {
+        b = Buffer.concat([b, chunk]);
+    }
+    return b.toString();
+}
+
+async function drive_ui(data: string[]): Promise<{ db: AssignmentDB, output: string }> {
+    const db = new AssignmentDB();
+    const stdin = mockStdIn();
+    const stdout = new PassThrough();
+    const app = main(db, process.stdin, stdout);
+    await inputs(stdin, data);
+    await app;
+    const output = await readAll(stdout);
+    return { db, output };
+}
+
+describe("UI", () => {
+    test("basic set/get inputs", async () => {
+        const { db, output } = await drive_ui([
+            "SET 1 1234",
+            "GET 1",
+            "END",
+        ]);
+
+        expect(db.get('1')).toEqual('1234');
+        expect(output).toMatchSnapshot();
+    });
+
+    test("count basics", async () => {
+        const { db, output } = await drive_ui([
+            "SET 1 1234",
+            "COUNT 1234",
+            "COUNT 123",
+            "SET 2 1234",
+            "COUNT 1234",
+            "END",
+        ]);
+
+        expect(db.get('1')).toEqual('1234');
+        expect(output).toMatchSnapshot();
+    });
+
+    test("invalid commands", async () => {
+        const { output } = await drive_ui([
+            "hello",
+            "HELLO",
+            "GET",
+            "SET",
+            "SET X",
+            "DELETE",
+            "END",
+        ]);
+
+        expect(output).toMatchSnapshot();
+    });
+
+    test("commit", async () => {
+        const { db, output } = await drive_ui([
+            "COMMIT",
+            "SET 1 1234",
+            "BEGIN",
+            "GET 1",
+            "SET 1 123",
+            "BEGIN",
+            "SET 1 abc",
+            "COMMIT",
+            "GET 1",
+            "ROLLBACK",
+            "END",
+        ]);
+
+        expect(db.get('1')).toEqual('abc');
+        expect(output).toMatchSnapshot();
+
+    })
+
+    test("basic rollback UI", async () => {
+        const { db, output } = await drive_ui([
+            "ROLLBACK",
+            "SET 1 1234",
+            "BEGIN",
+            "GET 1",
+            "SET 1 123",
+            "GET 1",
+            "ROLLBACK",
+            "GET 1",
+            "END",
+        ]);
+
+        expect(db.get('1')).toEqual('1234');
+        expect(output).toMatchSnapshot();
+    });
+
+    test("delete", async () => {
+        const { db, output } = await drive_ui([
+            "SET 1 1234",
+            "GET 1",
+            "DELETE 1",
+            "GET 1",
+            "END",
+        ]);
+
+        expect(db.get('1')).toBeUndefined()
+        expect(output).toMatchSnapshot();
+    });
+})
